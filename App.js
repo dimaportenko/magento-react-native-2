24c74600ff29c422ad63a8ab0c80fb52bddd2595
Dmytro Portenko <troublesohard@gmail.com>
2020-04-25 22:01:01 +0300

    init RN project

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import HomeScreen from "./src/screens/home/HomeScreen";
+
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <HomeScreen />
+    </>
+  );
+};
+
+
+
+export default App;
---
+++ src/screens/home/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default () => {
+  return (
+    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
