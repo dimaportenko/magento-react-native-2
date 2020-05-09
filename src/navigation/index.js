af4ed32ac6bd4d98f84b3b63dc4d360e31825c15
Dmytro Portenko <troublesohard@gmail.com>
2020-05-09 15:33:30 +0300

    update initial store config navigation logic

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -7,54 +7,62 @@ import { CategoryScreen } from '../screens/category/CategoryScreen';
 import { ProductScreen } from '../screens/product/ProductScreen';
 import { HomeScreen } from '../screens/home/HomeScreen';
 
-import type { Product } from '../logic/types/magento';
+import type { Product, StoreConfig } from '../logic/types/magento';
+import { useStoreConfig } from '../logic/storeConfig/useStoreConfig';
 
 const Stack = createStackNavigator();
 
 export const RootStack = () => {
+  const storeConfig: ?StoreConfig = useStoreConfig();
+  
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name={types.HOME_SCREEN}
-        component={HomeScreen}
-        options={({ navigation, route }) => {
-          return {
-            headerShown: false,
-            headerBackTitleVisible: false,
-          };
-        }}
-      />
-      <Stack.Screen
-        name={types.CATEGORIES_SCREEN}
-        component={CategoriesScreen}
-        options={({ navigation, route }) => {
-          return {
-            title: route?.params?.title || 'Categories',
-            headerBackTitleVisible: false,
-          };
-        }}
-      />
-      <Stack.Screen
-        name={types.CATEGORY_SCREEN}
-        component={CategoryScreen}
-        options={({ navigation, route }) => {
-          return {
-            title: route?.params?.title || 'Categories',
-            headerBackTitleVisible: false,
-          };
-        }}
-      />
-      <Stack.Screen
-        name={types.PRODUCT_SCREEN}
-        component={ProductScreen}
-        options={({ navigation, route }) => {
-          const product: Product = route.params.product;
-          return {
-            title: product.name || 'Product',
-            headerBackTitleVisible: false,
-          };
-        }}
-      />
+      {
+        (storeConfig === null)
+        ? (<Stack.Screen
+            name={types.HOME_SCREEN}
+            component={HomeScreen}
+            options={({ navigation, route }) => {
+              return {
+                headerShown: false,
+                headerBackTitleVisible: false,
+              };
+            }}
+          />)
+        : (<>
+            <Stack.Screen
+              name={types.CATEGORIES_SCREEN}
+              component={CategoriesScreen}
+              options={({ navigation, route }) => {
+                return {
+                  title: route?.params?.title || 'Categories',
+                  headerBackTitleVisible: false,
+                };
+              }}
+            />
+            <Stack.Screen
+              name={types.CATEGORY_SCREEN}
+              component={CategoryScreen}
+              options={({ navigation, route }) => {
+                return {
+                  title: route?.params?.title || 'Categories',
+                  headerBackTitleVisible: false,
+                };
+              }}
+            />
+            <Stack.Screen
+              name={types.PRODUCT_SCREEN}
+              component={ProductScreen}
+              options={({ navigation, route }) => {
+                const product: Product = route.params.product;
+                return {
+                  title: product.name || 'Product',
+                  headerBackTitleVisible: false,
+                };
+              }}
+            />
+          </>)
+      }
     </Stack.Navigator>
   );
 };
