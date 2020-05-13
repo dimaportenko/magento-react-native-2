2571361b0e80bd88ad2ca7dc15a8f33302422810
Dmytro Portenko <troublesohard@gmail.com>
2020-05-13 23:24:41 +0300

    move apollo client from App.js

--- App.js
+++ App.js
@@ -11,36 +11,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { NavigationContainer } from '@react-navigation/native';
-import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
-import { LoaderScreen, Colors } from 'react-native-ui-lib';
+import { ApolloProvider } from '@apollo/client';
 import { RootStack } from './src/navigation';
-import { magentoConfig } from './magento.config';
 import { themeInit } from './src/theme';
 import { persistor, store } from './src/redux/store';
+import { HomeScreen } from './src/screens/home/HomeScreen';
+import { client } from './src/apollo/client';
 
 themeInit();
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: `${magentoConfig.baseUrl}graphql/`,
-  }),
-});
-
-const Loading = () => (
-  <LoaderScreen
-    color={Colors.blue30}
-    message="Loading..."
-    overlay
-  />
-);
 
 const App: () => React$Node = () => {
   return (
     <Provider store={store}>
       <ApolloProvider client={client}>
         <PersistGate
-          loading={<Loading />}
+          loading={<HomeScreen />}
           persistor={persistor}
         >
           <NavigationContainer>
---
+++ src/apollo/client.js
@@ -0,0 +1,8 @@
+/**
+ * @flow
+ * Created by Dima Portenko on 13.05.2020
+ */
+
+export const client = () => {
+
+}
